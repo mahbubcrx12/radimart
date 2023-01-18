2041beaf72a03ad7f2e310a7de4c6f2fdd499705
mostofashawon <mostofashawon.xavi@gmail.com>
2023-01-18 18:16:23 +0600

    many things

--- lib/model/home_page_model.dart
+++ lib/model/home_page_model.dart
@@ -46,13 +46,33 @@ class Content {
   int? id;
   String? title;
   String? imgUrl;
+  String? slug;
+  String? icon;
+  String? inner;
+  String? shopName;
+  String? logo;
+  String? slogan;
+  String? backgroundImage;
+  String? imageUrl;
+  String? discountText;
 
-  Content({this.id, this.title, this.imgUrl});
+  Content({this.id, this.title, this.imgUrl,this.icon,this.inner,this.imageUrl,
+    this.slug,this.backgroundImage,this.logo,this.shopName,this.slogan,this.discountText});
 
   Content.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     title = json['title'];
     imgUrl = json['imgUrl'];
+    icon=json['icon'];
+    inner=json['inner'];
+    slug=json['slug'];
+    backgroundImage=json['backgroundImage'];
+    logo=json['logo'];
+    shopName=json['shopName'];
+    slogan=json['slogan'];
+    imageUrl=json['imageUrl'];
+    discountText=json['discountText'];
+
   }
 
   Map<String, dynamic> toJson() {
@@ -60,6 +80,164 @@ class Content {
     data['id'] = this.id;
     data['title'] = this.title;
     data['imgUrl'] = this.imgUrl;
+    data['icon'] = this.icon;
+    data['inner'] = this.inner;
+    data['slug'] = this.slug;
+    data['backgroundImage'] = this.backgroundImage;
+    data['logo'] = this.logo;
+    data['shopName'] = this.shopName;
+    data['slogan'] = this.slogan;
+    data['imageUrl'] = this.imageUrl;
+    data['discountText'] = this.discountText;
+
     return data;
   }
 }
+//
+// class HomePageModel {
+//   HomePageModel({
+//     this.sectionType,
+//     this.sectionTitle,
+//     this.sectionIcon,
+//     this.bgColor,
+//     this.order,
+//     this.content,
+//   });
+//
+//   String? sectionType;
+//   String? sectionTitle;
+//   String ?sectionIcon;
+//   String ?bgColor;
+//   int ?order;
+//   dynamic content;
+// }
+//
+// class Content {
+//   Content({
+//     this.id,
+//     this.title,
+//     this.imgUrl,
+//     this.slug,
+//     this.icon,
+//     this.inner,
+//     this.shopName,
+//     this.logo,
+//     this.slogan,
+//     this.backgroundImage,
+//     this.homepageSectionId,
+//     this.vendorId,
+//     this.imageUrl,
+//     this.rating,
+//     this.reviews,
+//     this.brandId,
+//     this.categoryId,
+//     this.discountPrice,
+//     this.discountText,
+//   });
+//
+//   int? id;//
+//   String? title;//
+//   String? imgUrl;//
+//   String? slug;//
+//   String? icon;//
+//   String? inner;//
+//   String ?shopName;
+//   String ?logo;
+//   String ?slogan;
+//   String ?backgroundImage;
+//   String ?homepageSectionId;
+//   String ?vendorId;
+//   String ?imageUrl;
+//   String ?rating;
+//   String ?reviews;
+//   String ?brandId;
+//   String ?categoryId;
+//   String ?discountPrice;
+//   String ?discountText;
+//
+//   Content.fromJson(Map<String, dynamic> json) {
+//     id = json['id'];
+//     title = json['title'];
+//     imgUrl = json['imgUrl'];
+//     icon=json['icon'];
+//     slug=json['slug'];
+//     inner=json['inner'];
+//     shopName=json['shopName'];
+//     logo=json['logo'];
+//     slogan=json['slogan'];
+//     backgroundImage=json['backgroundImage'];
+//     homepageSectionId=json['homepageSectionId'];
+//     vendorId=json['vendorId'];
+//     imageUrl=json['imageUrl'];
+//     rating=json['rating'];
+//     reviews=json['reviews'];
+//     brandId=json['brandId'];
+//     categoryId=json['categoryId'];
+//     discountPrice=json['discountPrice'];
+//     discountText=json['discountText'];
+//   }
+//
+//   Map<String, dynamic> toJson() {
+//     final Map<String, dynamic> data = new Map<String, dynamic>();
+//     data['id'] = this.id;
+//     data['title'] = this.title;
+//     data['imgUrl'] = this.imgUrl;
+//     data['icon'] = this.icon;
+//     data['inner'] = this.inner;
+//     data['slug'] = this.slug;
+//     data['shopName'] = this.shopName;
+//     data['logo'] = this.logo;
+//     data['slogan'] = this.slogan;
+//     data['backgroundImage'] = this.backgroundImage;
+//     data['homepageSectionId'] = this.homepageSectionId;
+//     data['vendorId'] = this.vendorId;
+//     data['rating'] = this.rating;
+//     data['reviews'] = this.reviews;
+//     data['brandId'] = this.brandId;
+//     data['categoryId'] = this.categoryId;
+//     data['discountPrice'] = this.discountPrice;
+//     data['discountText'] = this.discountText;
+//     data['imageUrl'] = this.imageUrl;
+//     return data;
+//   }
+// }
+//
+// class PurpleContent {
+//   PurpleContent({
+//     this.id,
+//     this.logo,
+//     this.email,
+//     this.phone,
+//     this.systemName,
+//     this.title,
+//     this.copyright,
+//     this.address,
+//     this.language,
+//     this.timeZone,
+//     this.footerText,
+//     this.slogan,
+//     this.facebook,
+//     this.twitter,
+//     this.youtube,
+//     this.instagram,
+//     this.linkedIn,
+//   });
+//
+//   int ?id;
+//   String? logo;
+//   String? email;
+//   String? phone;
+//   String ?systemName;
+//   String ?title;
+//   String ?copyright;
+//   String ?address;
+//   String ?language;
+//   String ?timeZone;
+//   String ?footerText;
+//   String ?slogan;
+//   String ?facebook;
+//   String ?twitter;
+//   String ?youtube;
+//   String ?instagram;
+//   String ?linkedIn;
+// }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -12,74 +12,191 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ScrollController _scrollController = ScrollController();
+
+  _scrollToBottom() {
+    _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+  }
    //home page api called data storing
    List<HomePageModel> homepageData=[];
 
  @override
-  void didChangeDependencies() async{
+  void didChangeDependencies()async {
+    // TODO: implement didChangeDependencies
    homepageData=await fetchHomepage();
-   print("hhhhhhhhhhhhhhhhhh");
-   print(homepageData);
-   super.initState();
-    super.didChangeDependencies();
+   setState(() {});
+   super.didChangeDependencies();
   }
   @override
   Widget build(BuildContext context) {
    var size=MediaQuery.of(context).size;
-    return Scaffold(
+   WidgetsBinding.instance.addPostFrameCallback((_) => _scrollToBottom());
+   return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 2,
+        elevation: 0,
         title: Image.asset("asset/logo.png"),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Container(
-            height: size.height,
-            width: size.width,
-            decoration: BoxDecoration(
-                color: Colors.amber
-            ),
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: homepageData.length,
-                itemBuilder:(context,index){
-                  // var toDecode = "${homepageData[index].content}";
-                  // var decodedContent = jsonDecode(toDecode);
+      body: Container(
+        height: size.height,
+        width: size.width,
+        decoration: BoxDecoration(
+            color: Colors.white
+        ),
+        child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: 1,
+            itemBuilder:(context,index){
+              var sliderContent=homepageData[0].content;
+              var categoryContent=homepageData[1].content;
+              var shopsContent=homepageData[2].content;
+              var popularContent=homepageData[3].content;
+
               return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 300,
-                    color: Colors.black,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                      Text("${homepageData[index].sectionTitle}",
-                        style: TextStyle(
-                            color: Colors.white,
-                          fontSize: 20
-                        ),),
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: size.height*.35,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          controller: _scrollController,
+                          reverse: true,
+                          shrinkWrap: true,
+                          itemCount: sliderContent!.length,
+                          itemBuilder: (context,index){
+                            return Image.network("${sliderContent[index].imgUrl}",
+                              //height: size.height*.35,
+                              width: size.width,
+                              fit: BoxFit.fill,
+                            );
+                          }),
+                    ),
+                    SizedBox(height: 10,),
+                    Text("Categories",style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold
+                    ),),
+                   // SizedBox(height: 8,),
+                    Container(
+                      height: size.height*.15,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: categoryContent!.length,
+                          itemBuilder: (context,index){
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 6),
+                              child: CircleAvatar(
+                                radius: 36,
+                                backgroundColor: Colors.black,
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage("${categoryContent[index].icon}"),
 
-                        ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: "${homepageData[index].content!.reversed}".length,
-                            itemBuilder: (context,index){
-                          return Container(
-                            child: Text("${homepageData[index].content![index].title.toString()}",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 10
-                              ),),
-                          );
-                        })
+                                  radius: 35,
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                    Text("Shops",style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold
+                    ),),
+                    Container(
+                      height: size.height*.40,
+                      width: double.infinity,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: shopsContent!.length,
+                          itemBuilder: (context,index){
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                               children: [
+                                 // Text("${shopsContent[index].slug}"),
+                                 ClipRRect(
+                                   child: shopsContent[index].logo==null
+                                       ? Image.network("https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png",
+                                     height: size.height*.35,
+                                     width: size.width*.3,
+                                     fit: BoxFit.fill,
+                                   )
+                                   :Image.network("${shopsContent[index].logo}",
+                                     height: size.height*.35,
+                                     width: size.width*.3,
+                                     fit: BoxFit.fill,
+                                   ),
+                                   borderRadius: BorderRadius.circular(12),
+                                 ),
+                                 Text("${shopsContent[index].shopName}",style: TextStyle(
+                                   fontSize: 20,
+                                   fontWeight: FontWeight.w400
+                                 ),)
+                               ],
+                              ),
+                            );
+                          }),
+                    ),
+                    Text("Popular",style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold
+                    ),),
+                    Container(
+                      height: size.height*.7,
+                      width: double.infinity,
+                      child:GridView.builder(
+                        scrollDirection: Axis.horizontal,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                          ),
+                          itemCount: popularContent!.length,
+                          itemBuilder: (context,index){
+                            return Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Container(
+height: size.height,
+                                child: Column(children: [
+                                  ClipRRect(
+                                    child: popularContent[index].imgUrl==null
+                                        ? Image.network("https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png",
+                                      height: size.height*.25,
+                                      width: size.width*.3,
+                                      fit: BoxFit.fitWidth,
+                                    )
+                                        :Image.network("${popularContent[index].imageUrl}",
+                                      height: size.height*.20,
+                                      width: size.width*.25,
+                                      fit: BoxFit.fill,
+                                    ),
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
 
-                    ],),
+                                  Text("${popularContent[index].title}",style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black
+                                  ),),
+                                  Text("${popularContent[index].discountText}",style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.red
+                                  ),)
+                                ],),
+                              ),
+                            );
+                          }
+                      )
+                    )
+                  ],
                 ),
               );
             }),
-          )
-        ],),
       ),
     );
   }
