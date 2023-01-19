df70647c93acf4bf4f7dd584658b5e87f86b1a4f
mostofashawon <mostofashawon.xavi@gmail.com>
2023-01-19 18:13:34 +0600

    many things

--- lib/controller/service/homepage_service.dart
+++ lib/controller/service/homepage_service.dart
@@ -1,25 +1,29 @@
 import 'dart:convert';
-
 import 'package:radimart_rdtl/model/home_page_model.dart';
 import 'package:http/http.dart' as http;
 
 Future<List<HomePageModel>> fetchHomepage() async {
   List<HomePageModel> homepageData = [];
+
   try {
     var link = "http://public.radimart.com/api/v1/homepage";
     var response =
-    await http.get(Uri.parse(link),);
+    await http.get(Uri.parse(link),headers: {
+      "Content-Type": "application/json",
+    });
 
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
 
       HomePageModel homepageModel;
       for (var i in data) {
+
         homepageModel = HomePageModel.fromJson(i);
         homepageData.add(homepageModel);
       }
-      print("cccccccccccccccccccccccccccccc}");
+
       print(homepageData);
+
       return homepageData;
     } else {
       return homepageData;
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,203 +1,225 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:radimart_rdtl/controller/service/homepage_service.dart';
 import 'package:radimart_rdtl/model/home_page_model.dart';
 
 class HomePage extends StatefulWidget {
-   HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  ScrollController _scrollController = ScrollController();
+  // ScrollController _scrollController = ScrollController();
+  //
+  // _scrollToBottom() {
+  //   _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+  // }
+  //home page api called data storing
 
-  _scrollToBottom() {
-    _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
-  }
-   //home page api called data storing
-   List<HomePageModel> homepageData=[];
+  List<HomePageModel> homepageData = [];
 
- @override
-  void didChangeDependencies()async {
-    // TODO: implement didChangeDependencies
-   homepageData=await fetchHomepage();
-   setState(() {});
-   super.didChangeDependencies();
+
+  // var categoryContent=Null;
+  // var shopsContent=Null;
+  // var popularContent=Null;
+
+  @override
+  void didChangeDependencies() async {
+    print('hhhhhhhhhhhhhh');
+    print(homepageData);
+    print('hhhhhhhhhhhhhh');
+    homepageData = await fetchHomepage();
+    setState(() {
+
+    });
+    print('xxxxxxxxxxx');
+    print(homepageData);
+    print('xxxxxxxxxxx');
+    super.didChangeDependencies();
   }
+
   @override
   Widget build(BuildContext context) {
-   var size=MediaQuery.of(context).size;
-   WidgetsBinding.instance.addPostFrameCallback((_) => _scrollToBottom());
-   return Scaffold(
+    var size = MediaQuery.of(context).size;
+    var sliderContent = homepageData[0].content;
+    var categoryContent = homepageData[1].content;
+    var shopsContent = homepageData[2].content;
+    var popularContent = homepageData[3].content;
+
+
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
         title: Image.asset("asset/logo.png"),
         centerTitle: true,
       ),
-      body: Container(
-        height: size.height,
-        width: size.width,
-        decoration: BoxDecoration(
-            color: Colors.white
-        ),
-        child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: 1,
-            itemBuilder:(context,index){
-              var sliderContent=homepageData[0].content;
-              var categoryContent=homepageData[1].content;
-              var shopsContent=homepageData[2].content;
-              var popularContent=homepageData[3].content;
-
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: size.height*.35,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          controller: _scrollController,
-                          reverse: true,
-                          shrinkWrap: true,
-                          itemCount: sliderContent!.length,
-                          itemBuilder: (context,index){
-                            return Image.network("${sliderContent[index].imgUrl}",
-                              //height: size.height*.35,
-                              width: size.width,
-                              fit: BoxFit.fill,
-                            );
-                          }),
-                    ),
-                    SizedBox(height: 10,),
-                    Text("Categories",style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold
-                    ),),
-                   // SizedBox(height: 8,),
-                    Container(
-                      height: size.height*.15,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          shrinkWrap: true,
-                          itemCount: categoryContent!.length,
-                          itemBuilder: (context,index){
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 6),
-                              child: CircleAvatar(
-                                radius: 36,
-                                backgroundColor: Colors.black,
-                                child: CircleAvatar(
-                                  backgroundImage: NetworkImage("${categoryContent[index].icon}"),
-
-                                  radius: 35,
-                                ),
-                              ),
-                            );
-                          }),
-                    ),
-                    Text("Shops",style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold
-                    ),),
-                    Container(
-                      height: size.height*.40,
-                      width: double.infinity,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          shrinkWrap: true,
-                          itemCount: shopsContent!.length,
-                          itemBuilder: (context,index){
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 8),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                               children: [
-                                 // Text("${shopsContent[index].slug}"),
-                                 ClipRRect(
-                                   child: shopsContent[index].logo==null
-                                       ? Image.network("https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png",
-                                     height: size.height*.35,
-                                     width: size.width*.3,
-                                     fit: BoxFit.fill,
-                                   )
-                                   :Image.network("${shopsContent[index].logo}",
-                                     height: size.height*.35,
-                                     width: size.width*.3,
-                                     fit: BoxFit.fill,
-                                   ),
-                                   borderRadius: BorderRadius.circular(12),
-                                 ),
-                                 Text("${shopsContent[index].shopName}",style: TextStyle(
-                                   fontSize: 20,
-                                   fontWeight: FontWeight.w400
-                                 ),)
-                               ],
-                              ),
-                            );
-                          }),
-                    ),
-                    Text("Popular",style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold
-                    ),),
-                    Container(
-                      height: size.height*.7,
-                      width: double.infinity,
-                      child:GridView.builder(
-                        scrollDirection: Axis.horizontal,
-                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: size.height * .3,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: sliderContent!.length,
+                    itemBuilder: (context, index) {
+                      return Image.network(
+                        "${sliderContent![index].imgUrl}",
+                        //height: size.height*.35,
+                        width: size.width,
+                        fit: BoxFit.fill,
+                      );
+                    }),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Categories",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              // SizedBox(height: 8,),
+              Container(
+                height: size.height * .15,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount:categoryContent != null ? categoryContent.length : 0,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding:
+                            const EdgeInsets.symmetric(horizontal: 6),
+                        child: CircleAvatar(
+                          radius: 36,
+                          backgroundColor: Colors.black,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "${categoryContent![index].icon}"),
+                            radius: 35,
                           ),
-                          itemCount: popularContent!.length,
-                          itemBuilder: (context,index){
-                            return Padding(
-                              padding: const EdgeInsets.all(2.0),
-                              child: Container(
-height: size.height,
-                                child: Column(children: [
-                                  ClipRRect(
-                                    child: popularContent[index].imgUrl==null
-                                        ? Image.network("https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png",
-                                      height: size.height*.25,
-                                      width: size.width*.3,
-                                      fit: BoxFit.fitWidth,
+                        ),
+                      );
+                    }),
+              ),
+              Text(
+                "Shops",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                height: size.height * .40,
+                width: double.infinity,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: shopsContent != null ? shopsContent.length : 0,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding:
+                            const EdgeInsets.symmetric(horizontal: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // Text("${shopsContent[index].slug}"),
+                            ClipRRect(
+                              child: shopsContent![index].logo == null
+                                  ? Image.network(
+                                      "https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png",
+                                      height: size.height * .35,
+                                      width: size.width * .3,
+                                      fit: BoxFit.fill,
                                     )
-                                        :Image.network("${popularContent[index].imageUrl}",
-                                      height: size.height*.20,
-                                      width: size.width*.25,
+                                  : Image.network(
+                                      "${shopsContent![index].logo}",
+                                      height: size.height * .35,
+                                      width: size.width * .3,
                                       fit: BoxFit.fill,
                                     ),
-                                    borderRadius: BorderRadius.circular(12),
-                                  ),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            Text(
+                              "${shopsContent![index].shopName}",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w400),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              Text(
+                "Popular",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                   height: size.height * .7,
+                   width: double.infinity,
 
-                                  Text("${popularContent[index].title}",style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black
-                                  ),),
-                                  Text("${popularContent[index].discountText}",style: TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.red
-                                  ),)
-                                ],),
-                              ),
-                            );
-                          }
-                      )
-                    )
-                  ],
-                ),
-              );
-            }),
-      ),
+                  child: GridView.builder(
+                      scrollDirection: Axis.horizontal,
+                      gridDelegate:
+                          SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                      ),
+                      itemCount: popularContent != null ? popularContent.length : 0,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Container(
+
+                            child: Column(
+                              children: [
+                                ClipRRect(
+                                  child:
+                                      popularContent![index].imgUrl == null
+                                          ? Image.network(
+                                              "https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png",
+                                             // height: size.height*.2,
+                                             //   width: size.width*.5,
+                                              fit: BoxFit.cover,
+                                            )
+                                          : Image.network(
+                                              "${popularContent[index].imageUrl}",
+                                              // height: size.height*.2,
+                                              // width: size.width*.5,
+                                              fit: BoxFit.cover,
+                                            ),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                Text(
+                                  "${popularContent[index].title}",
+                                  style: TextStyle(
+                                      fontSize: 14, color: Colors.black),
+                                  maxLines: 1,
+                                ),
+                                Text(
+                                  "${popularContent[index].discountText}",
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.red),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }))
+            ],
+          ),
+        ),
+      )
     );
   }
 }
